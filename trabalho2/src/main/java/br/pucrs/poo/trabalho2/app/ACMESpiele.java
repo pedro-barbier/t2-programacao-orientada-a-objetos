ed13e597e7380a34fcef942eaa9a68abe32116a8
barbier1 <162060319+pedro-barbier@users.noreply.github.com>
2026-06-15 23:19:49 +0000

    chore: Estrutura inicial do projeto (Spring Boot + Vaadin)

---
+++ trabalho2/src/main/java/br/pucrs/poo/trabalho2/app/ACMESpiele.java
@@ -0,0 +1,5 @@
+package br.pucrs.poo.trabalho2.app;
+
+public class ACMESpiele {
+
+}
---
+++ trabalho2/src/test/java/br/pucrs/poo/trabalho2/Trabalho2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.poo.trabalho2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
