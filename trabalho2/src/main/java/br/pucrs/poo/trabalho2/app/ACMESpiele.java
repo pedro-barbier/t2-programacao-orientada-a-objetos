09d8cd8fd2bbe67a30d2a2fbcf96f64b78265635
pedro-barbier <pedro.barbieri@edu.pucrs.br>
2026-06-29 20:25:38 -0300

    refactor: Adição de eventos para grande parte dos botões da página inicial. Está faltando ainda o salvamento e carregamento de dados.

--- trabalho2/src/main/java/br/pucrs/poo/trabalho2/app/ACMESpiele.java
+++ trabalho2/src/main/java/br/pucrs/poo/trabalho2/app/ACMESpiele.java
@@ -1,11 +1,13 @@
 package br.pucrs.poo.trabalho2.app;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -27,43 +29,81 @@ public class ACMESpiele extends VerticalLayout {
         Text titulo = new Text("ACME Spiele");
         add(titulo);
 
+        //// AREA DE CADASTRO (1-4)
         Text subtitulo = new Text("Area de cadastro");
-        Button cadCliente = new Button("Cadastrar clientes");
-        Button cadJogo = new Button("Cadastrar jogos");
-        Button cadFormaPagamento = new Button("Cadastrar formas de pagamento");
-        Button cadContrato = new Button("Cadastrar contratos");
+        Button cadCliente = new Button("Cadastrar clientes", e -> {
+            UI.getCurrent().navigate("cadastro-cliente");
+        });
+        Button cadJogo = new Button("Cadastrar jogos", e -> {
+            UI.getCurrent().navigate("cadastro-jogo");
+        });
+        Button cadFormaPagamento = new Button("Cadastrar formas de pagamento", e -> {
+            UI.getCurrent().navigate("cadastro-forma-pagamento");
+        });
+        Button cadContrato = new Button("Cadastrar contratos", e -> {
+            UI.getCurrent().navigate("cadastro-contrato");
+        });
         VerticalLayout area1 = new VerticalLayout(subtitulo, cadCliente, cadJogo, cadFormaPagamento, cadContrato);
         add(area1);
 
+        //// AREA DE RELATORIOS (5-7)
         Text subtitulo2 = new Text("Area de relatorios");
-        Button relatorioJogos = new Button("Relatorio de jogos");
-        Button relatorioClientes = new Button("Relatorio de clientes");
-        Button relatorioContratos = new Button("Relatorio de contratos");
+        Button relatorioJogos = new Button("Relatorio de jogos", e -> {
+            UI.getCurrent().navigate("relatorio-jogos");
+        });
+        Button relatorioClientes = new Button("Relatorio de clientes", e -> {
+            UI.getCurrent().navigate("relatorio-clientes");
+        });
+        Button relatorioContratos = new Button("Relatorio de contratos", e -> {
+            UI.getCurrent().navigate("relatorio-contratos");
+        });
         VerticalLayout area2 = new VerticalLayout(subtitulo2, relatorioJogos, relatorioClientes, relatorioContratos);
         add(area2);
 
+        //// AREA ADMINISTRATIVA (8-10)
         Text subtitulo3 = new Text("Area administrativa");
-        Button removerContrato = new Button("Remover contrato");
-        Button alterarCliente = new Button("Alterar cliente");
-        Button consultar = new Button("Consultar");
+        Button removerContrato = new Button("Remover contrato", e -> {
+            UI.getCurrent().navigate("remover-contrato");
+        });
+        Button alterarCliente = new Button("Alterar cliente", e -> {
+            UI.getCurrent().navigate("alterar-cliente");
+        });
+        Button consultar = new Button("Consultar", e -> {
+            UI.getCurrent().navigate("consultas");
+        });
         VerticalLayout area3 = new VerticalLayout(subtitulo3, removerContrato, alterarCliente, consultar);
         add(area3);
 
+        //// AREA DE SALVAR/CARREGAR DADOS (11-12)
         Button salvarDados = new Button("Salvar dados");
         Button carregarDados = new Button("Carregar dados");
         HorizontalLayout botoes = new HorizontalLayout(salvarDados, carregarDados);
         add(botoes);
 
-        Button sair = new Button("Finalizar sistema");
+        Button sair = new Button("Finalizar sistema", e -> {
+            VaadinSession.getCurrent().getSession().invalidate();
+            VaadinSession.getCurrent().close();
+            System.exit(0);
+        });
         add(sair);
 
     }
 
+    /*
+        O método inicializar() da classe ACMESpiele deve ler dados de arquivos de entrada para
+        cadastrar alguns clientes, jogos e contratos iniciais (os formatos dos arquivos de entrada são
+        apresentados no Apêndice do enunciado). Cada contrato lido do arquivo de contratos deve ser
+        armazenado em uma fila. Após a leitura de todos os contratos do arquivo, cada contrato válido
+        deve ser cadastrado no sistema
+        Todo cliente precisa se cadastrar com seus dados; e pode ser individual (pessoa física) ou
+        corporativo (pessoa jurídica). O método descrever() deve gerar uma String com os valores dos
+        atributos separados por (;) ponto-e-vírgula.
+        A empresa disponibiliza diversos jogos eletrônicos exclusivos (podem ser contratados apenas
+        por um cliente de cada vez) para serem contratados para serem jogados por um determinado
+        período (em dias) a partir da data do contrato.
+        No contrato é definida uma forma de pagamento.
+     */
     public void inicializar() {
-
-    }
-
-    public void carregarDados() {
         // CONTRATOS
         Queue<Contrato> contratos = new LinkedList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/dados/CONTRATOSINICIAL.csv"))) {
